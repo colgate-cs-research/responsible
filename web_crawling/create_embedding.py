64af22f2bb8f8fca0788ef84752c6d41afe0ad01
Emily Huff <emilyhuff@Emilys-MacBook-Pro-2.local>
2020-04-13 16:30:54 -0500

    embedding attempts and no neutral labels (slightly better)

---
+++ web_crawling/create_embedding.py
@@ -0,0 +1,122 @@
+# https://towardsdatascience.com/word-embeddings-for-sentiment-analysis-65f42ea5d26e
+
+import pandas as pd
+import numpy as np
+import re
+import collections
+import matplotlib.pyplot as plt
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+from nltk.corpus import stopwords
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils.np_utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+from keras import models
+from keras import layers
+
+def deep_model(model, X_train, y_train, X_valid, y_valid):
+    '''
+    Function to train a multi-class model. The number of epochs and 
+    batch_size are set by the constants at the top of the
+    notebook. 
+    
+    Parameters:
+        model : model with the chosen architecture
+        X_train : training features
+        y_train : training target
+        X_valid : validation features
+        Y_valid : validation target
+    Output:
+        model training history
+    '''
+    model.compile(optimizer='rmsprop'
+                  , loss='categorical_crossentropy'
+                  , metrics=['accuracy'])
+    
+    history = model.fit(X_train
+                       , y_train
+                       , epochs=NB_START_EPOCHS
+                       , batch_size=BATCH_SIZE
+                       , validation_data=(X_valid, y_valid)
+                       , verbose=0)
+    return history
+
+def test_model(model, X_train, y_train, X_test, y_test, epoch_stop):
+    '''
+    Function to test the model on new data after training it
+    on the full training data with the optimal number of epochs.
+    
+    Parameters:
+        model : trained model
+        X_train : training features
+        y_train : training target
+        X_test : test features
+        y_test : test target
+        epochs : optimal number of epochs
+    Output:
+        test accuracy and test loss
+    '''
+    model.fit(X_train
+              , y_train
+              , epochs=epoch_stop
+              , batch_size=BATCH_SIZE
+              , verbose=0)
+    results = model.evaluate(X_test, y_test)
+    
+    return results
+
+NB_WORDS = 10000  # Parameter indicating the number of words we'll put in the dictionary
+VAL_SIZE = 1000  # Size of the validation set
+NB_START_EPOCHS = 10  # Number of epochs we usually start to train with
+BATCH_SIZE = 512  # Size of the batches used in the mini-batch gradient descent
+MAX_LEN = 24  # Maximum number of words in a sequence
+
+df = pd.read_csv('aligned_labeled_data.csv', header= None)
+df = df.rename(index=str, columns={ 0: "text", 1: "aspect_category", 2: "sentiment", 3: "predicted_AS"})
+
+# assume all are labeled as NET_NEUTRAL
+df = df.drop(["predicted_AS"], axis=1)
+df = df.drop(["aspect_category"], axis=1)
+
+indeces_none = df[df['sentiment'] == 'NONE'].index
+df.drop(indeces_none, inplace=True)
+
+X_train, X_test, y_train, y_test = train_test_split(df.text, df.sentiment, test_size=0.10)
+
+tk = Tokenizer(num_words=NB_WORDS,
+filters='!"#$%&()*+,-./:;<=>?@[\]^_`{"}~\t\n',lower=True, split=" ")
+tk.fit_on_texts(X_train)
+X_train_seq = tk.texts_to_sequences(X_train)
+X_test_seq = tk.texts_to_sequences(X_test)
+
+X_train_seq_trunc = pad_sequences(X_train_seq, maxlen=MAX_LEN)
+X_test_seq_trunc = pad_sequences(X_test_seq, maxlen=MAX_LEN)
+
+le = LabelEncoder()
+y_train_le = le.fit_transform(y_train)
+y_test_le = le.transform(y_test)
+y_train_oh = to_categorical(y_train_le)
+y_test_oh = to_categorical(y_test_le)
+
+X_train_emb, X_valid_emb, y_train_emb, y_valid_emb = train_test_split(X_train_seq_trunc, y_train_oh, test_size=0.1, random_state=37)
+
+emb_model = models.Sequential()
+emb_model.add(layers.Embedding(NB_WORDS, 8, input_length=MAX_LEN))
+emb_model.add(layers.Flatten())
+emb_model.add(layers.Dense(3, activation='softmax'))
+emb_history = deep_model(emb_model, X_train_emb, y_train_emb, X_valid_emb, y_valid_emb)
+
+emb_results = test_model(emb_model, X_train_seq_trunc, y_train_oh, X_test_seq_trunc, y_test_oh, 6)
+print('/n')
+print('Test accuracy of word embeddings model: {0:.2f}%'.format(emb_results[1]*100))
+
+
+
+
+
+
+
+
+
+
---
+++ web_crawling/sentiment_adaptation.py
@@ -0,0 +1,132 @@
+import spacy
+import pandas as pd
+import numpy as np
+from keras.models import load_model
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.preprocessing.text import Tokenizer
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import to_categorical
+from collections import Counter
+from sklearn.model_selection import train_test_split
+
+nlp = spacy.load('en')
+
+df = pd.read_csv('aligned_labeled_data.csv', header= None)
+df = df.rename(index=str, columns={ 0: "text", 1: "aspect_category", 2: "sentiment", 3: "predicted_AS"})
+
+# assume all are labeled as NET_NEUTRAL
+df = df.drop(["predicted_AS"], axis=1)
+df = df.drop(["aspect_category"], axis=1)
+
+indeces_none = df[df['sentiment'] == 'NONE'].index
+df.drop(indeces_none, inplace=True)
+
+## TRY DROPPING NEUTRAL LABEL
+indeces_neutral = df[df['sentiment'] == 'NEUTRAL'].index
+df.drop(indeces_neutral, inplace=True)
+
+
+print(Counter(df["sentiment"]).keys())
+
+X_train, X_test = train_test_split(df, test_size=0.10)
+
+### attempt using SENTIMENT PORTION
+
+sentiment_terms = []
+for review in nlp.pipe(X_train.text):
+        if review.is_parsed:
+            sentiment_terms.append(' '.join([token.lemma_ for token in review if (not token.is_stop and not token.is_punct and (token.pos_ == "ADJ" or token.pos_ == "VERB" or token.pos_ == "PART"))]))
+        else:
+            sentiment_terms.append('')  
+X_train['sentiment_terms'] = sentiment_terms
+# print(sentiment_terms)
+
+vocab_size = 6000 # We set a maximum size for the vocabulary
+sentiment_model = Sequential()
+sentiment_model.add(Dense(512, input_shape=(6000,), activation='relu'))
+# positive, negative, neutral-> 3
+# WITHOUT NEUTRAL -> 2
+sentiment_model.add(Dense(2, activation='softmax')) 
+sentiment_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+tokenizer = Tokenizer(num_words=vocab_size)
+tokenizer.fit_on_texts(X_train.text)
+sentiment_tokenized = pd.DataFrame(tokenizer.texts_to_matrix(X_train.sentiment_terms))
+
+label_encoder_2 = LabelEncoder()
+integer_sentiment = label_encoder_2.fit_transform(X_train.sentiment)
+dummy_sentiment = to_categorical(integer_sentiment)
+
+sentiment_model.fit(sentiment_tokenized, dummy_sentiment, epochs=5, verbose=1)
+
+test_reviews = X_test.text
+                            
+# Sentiment preprocessing
+test_sentiment_terms = []
+for review in nlp.pipe(test_reviews):
+        if review.is_parsed:
+            test_sentiment_terms.append(' '.join([token.lemma_ for token in review if (not token.is_stop and not token.is_punct and (token.pos_ == "ADJ" or token.pos_ == "VERB"))]))
+        else:
+            test_sentiment_terms.append('') 
+test_sentiment_terms = pd.DataFrame(tokenizer.texts_to_matrix(test_sentiment_terms))
+
+# Models output
+test_sentiment = label_encoder_2.inverse_transform(sentiment_model.predict_classes(test_sentiment_terms))
+j = 0
+
+sentiment_model_accuracy = 0
+
+pos_but_neg = 0
+neutral_but_neg = 0
+
+pos_but_neutral = 0
+neg_but_neutral = 0
+
+neg_but_pos = 0
+neutral_but_pos = 0
+
+num_errors = 0
+
+while j < len(X_test.sentiment):
+    print()
+    print(test_reviews[j])
+    print("Sentiment model answer: Review " + str(j+1) + " is expressing a  " + test_sentiment[j] + " opinion about net neutrality")
+    print("True answer: Review " + str(j+1) + " is expressing a  " + X_test.iloc[j, 1] + " opinion about net neutrality")
+    print()
+    if test_sentiment[j] == X_test.iloc[j, 1]:
+        sentiment_model_accuracy += 1
+    else:
+        num_errors += 1
+        if test_sentiment[j] == 'NEUTRAL':
+            if X_test.iloc[j, 1] == 'POSITIVE':
+                pos_but_neutral += 1
+            else: # guessed negative
+                neg_but_neutral += 1
+        elif test_sentiment[j] == 'NEGATIVE':
+            if X_test.iloc[j, 1] == 'POSITIVE':
+                pos_but_neg += 1
+            else: # guessed neutral
+                neutral_but_neg += 1
+        else: # correst sentiment is positive
+            if X_test.iloc[j, 1] == 'NEGATIVE':
+                neg_but_pos += 1
+            else: # guessed neutral
+                neutral_but_pos += 1
+    j += 1
+
+
+print("sentiment model accuracy: {}".format(sentiment_model_accuracy/j))
+# print("Breakdown of errors:")
+# print("######################")
+# print("Guessed negative when positive: {}".format(neg_but_pos/j))
+# print("######################")
+# print("Guessed negative when neutral: {}".format(neg_but_neutral/j))
+# print("Guessed neutral when positive: {}".format(neutral_but_pos/j))
+# print("Guessed neutral when negative: {}".format(neutral_but_neg/j))
+# print("######################")
+# print("Guessed positive when negative: {}".format(pos_but_neg/j))
+# print("######################")
+# print("Guessed positive when neutral: {}".format(pos_but_neutral/j))
+
